f2370a1e013cb3e92d4e4c683d1e81ebf0ea3611
gitmumca <gitmumca@mail.ru>
2022-05-26 17:00:09 +0300

    feat: edit 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const pathStyle = path.join(__dirname, 'styles');
 const pathProject = path.join(__dirname, 'project-dist', 'bundle.css');
@@ -6,16 +5,26 @@ const { stdout } = process;
 
 const fs = require('fs');
 fs.readdir(pathStyle, { withFileTypes: true }, function(err, items) {
-  if (err) throw err;      
-  items.forEach(function(el) {
-    if (path.extname(el.name) == '.css') {
-      fs.readFile(path.join(pathStyle, el.name), 'utf8', function (err, data) {
-        if (err) throw err;
-        fs.appendFile(pathProject, '\n' + data.toString(), err => {
-          if (err) throw err; 
-          stdout.write(`Файл успешно добавлен ${el.name}\n`);
+    if (err) throw err;      
+    items.forEach(function(el, index) {
+      if (path.extname(el.name) == '.css') {
+        fs.readFile(path.join(pathStyle, el.name), 'utf8', function (err, data) {
+          if (err) throw err;
+          if (index === 0){
+            fs.writeFile(pathProject, '\n' + data.toString(), err => {
+              if (err) throw err; 
+              stdout.write(`Файл успешно добавлен ${el.name}\n`);
+            });
+          }
+          else {
+            fs.appendFile(pathProject, '\n' + data.toString(), err => {
+              if (err) throw err; 
+              stdout.write(`Файл успешно добавлен ${el.name}\n`);
+            });
+          }
         });
-      });
-    }
-  });
+      }
+    });
 });  
+
+
